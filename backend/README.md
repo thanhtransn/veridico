927e1d860cdc25c8ca8d9e6e1243d6335b679e36
Tran Van Thanh <thanhtransn1708@gmail.com>
2026-01-01 16:44:13 +0700

    update init data

--- backend/README.md
+++ backend/README.md
@@ -5,7 +5,7 @@
     - script: 
       - docker pull postgres:16
       - docker run --name postgres-db -e POSTGRES_DB=veridico -e POSTGRES_USER=postgres -e POSTGRES_PASSWORD=thanh1708 -p 5432:5432 -d postgres:16
-      - docker exec -i postgres-db psql -U postgres -d veridico < src/database/seed/init.sql (Window)
+      - docker exec -i postgres-db psql -U postgres -d veridico < backend/src/database/seed/init.sql (Window)
       - yarn db:migration
       - yarn start
 - Test
--- backend/src/database/seed/init.sql
+++ backend/src/database/seed/init.sql
@@ -1,2 +1,13 @@
 INSERT INTO "user" (first_name, last_name, email, password, mobile_number, role)
-VALUES ('Super', 'Admin', 'superadmin@gmail.com', '$2a$12$sfEpSFAUNm7W/gMIHdUQyu.ZvI.ePQNEhVJfDiWnoH7m/NCE7ajy.', '0362163467', 'superadmin');
+VALUES 
+('Super', 'Admin', 'superadmin@gmail.com', '$2a$12$sfEpSFAUNm7W/gMIHdUQyu.ZvI.ePQNEhVJfDiWnoH7m/NCE7ajy.', '0362163467', 'superadmin'),
+('test', 'QA', 'test@gmail.com', '$2a$12$sfEpSFAUNm7W/gMIHdUQyu.ZvI.ePQNEhVJfDiWnoH7m/NCE7ajy.', '0362163962', 'user'),
+('tes1t', 'QA', 'test1@gmail.com', '$2a$12$sfEpSFAUNm7W/gMIHdUQyu.ZvI.ePQNEhVJfDiWnoH7m/NCE7ajy.', '0362167362', 'user'),
+('test2', 'QA', 'test2@gmail.com', '$2a$12$sfEpSFAUNm7W/gMIHdUQyu.ZvI.ePQNEhVJfDiWnoH7m/NCE7ajy.', '0364143462', 'user'),
+('test3', 'QA', 'test3@gmail.com', '$2a$12$sfEpSFAUNm7W/gMIHdUQyu.ZvI.ePQNEhVJfDiWnoH7m/NCE7ajy.', '0367163462', 'user'),
+('test4', 'QA', 'test4@gmail.com', '$2a$12$sfEpSFAUNm7W/gMIHdUQyu.ZvI.ePQNEhVJfDiWnoH7m/NCE7ajy.', '0361163462', 'user'),
+('test5', 'QA', 'test5@gmail.com', '$2a$12$sfEpSFAUNm7W/gMIHdUQyu.ZvI.ePQNEhVJfDiWnoH7m/NCE7ajy.', '0366163462', 'user'),
+('test6', 'QA', 'test6@gmail.com', '$2a$12$sfEpSFAUNm7W/gMIHdUQyu.ZvI.ePQNEhVJfDiWnoH7m/NCE7ajy.', '0362163262', 'user'),
+('test7', 'QA', 'test7@gmail.com', '$2a$12$sfEpSFAUNm7W/gMIHdUQyu.ZvI.ePQNEhVJfDiWnoH7m/NCE7ajy.', '0362163462', 'user'),
+('test8', 'QA', 'test8@gmail.com', '$2a$12$sfEpSFAUNm7W/gMIHdUQyu.ZvI.ePQNEhVJfDiWnoH7m/NCE7ajy.', '0362168462', 'user'),
+('test9', 'QA', 'test9@gmail.com', '$2a$12$sfEpSFAUNm7W/gMIHdUQyu.ZvI.ePQNEhVJfDiWnoH7m/NCE7ajy.', '0362167462', 'user');
